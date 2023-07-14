975d1e355ebd7fa29918eb7b1246f78811d973e1
Dohun Lee <nestour95@gmail.com>
2023-07-14 13:24:20 +0900

    solved : 23.07.14

---
+++ src/baekjoon/No_3003.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class No_3003 {
+
+    static int[] cnt = {1, 1, 2, 2, 2, 8};
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < 6; i++) {
+            sb.append(cnt[i] - Integer.parseInt(st.nextToken()))
+                    .append(" ");
+        }
+        System.out.println(sb);
+    }
+}
