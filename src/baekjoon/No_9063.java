5948c8059dbcde3503ac4f38db4c01d75e8d3d7e
Dohun Lee <nestour95@gmail.com>
2023-07-16 00:26:54 +0900

    solved : 23.07.16

--- src/baekjoon/No_9063.java
+++ src/baekjoon/No_9063.java
@@ -1,14 +1,35 @@
 package baekjoon;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class No_9063 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+		int x1 = Integer.MAX_VALUE;
+		int x2 = Integer.MIN_VALUE;
+		int y1 = Integer.MAX_VALUE;
+		int y2 = Integer.MIN_VALUE;
 
+		int N = Integer.parseInt(br.readLine());
+
+		StringTokenizer st;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+
+			x1 = Math.min(x1, x);
+			x2 = Math.max(x2, x);
+			y1 = Math.min(y1, y);
+			y2 = Math.max(y2, y);
+		}
+
+		System.out.println((x2 - x1) * (y2 -y1));
 	}
 
 }
