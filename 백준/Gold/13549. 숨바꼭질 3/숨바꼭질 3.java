9723f2b031d3d2d40e05ad10b68d26f0d9e95886
Dohun Lee <nestour95@gmail.com>
2023-08-03 09:14:45 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 180 ms, Memory: 40104 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static int N;
+    static int K;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        int big = 200000;
+        int[] arr = new int[200001];
+
+        ArrayDeque<Node> queue = new ArrayDeque<>();
+
+        Arrays.fill(arr, Integer.MAX_VALUE);
+
+        queue.add(new Node(N, 0));
+
+        int min = Integer.MAX_VALUE;
+        while (!queue.isEmpty()) {
+            Node cur = queue.poll();
+
+            if(arr[cur.x] <= cur.time) continue;
+
+            arr[cur.x] = cur.time;
+
+            if (cur.x - 1 >= 0) {
+                queue.add(new Node(cur.x - 1, cur.time + 1));
+            }
+
+            if (cur.x + 1 <= big) {
+                queue.add(new Node(cur.x + 1, cur.time + 1));
+            }
+
+            if (cur.x * 2 <= big) {
+                queue.add(new Node(cur.x * 2, cur.time));
+            }
+        }
+
+        System.out.println(arr[K]);
+    }
+
+    static class Node{
+        int x;
+        int time;
+
+        public Node(int x, int time) {
+            this.x = x;
+            this.time = time;
+        }
+    }
+}
