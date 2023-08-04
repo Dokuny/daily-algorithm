15aa1cafdf78bcd9055368202f3549c831587980
Dohun Lee <nestour95@gmail.com>
2023-08-04 14:00:55 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 123 ms, Memory: 20,192 KB -BaekjoonHub

---
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
+
+class Solution {
+    public static void main(String args[]) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        for (int test_case = 1; test_case <= 10; test_case++) {
+            br.readLine();
+
+            ArrayDeque<Integer> queue = new ArrayDeque<>();
+
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < 8; i++) {
+                queue.addLast(Integer.parseInt(st.nextToken()));
+            }
+
+            // cnt가 9이면 1로 변환
+            int cnt = 1;
+
+            while (true) {
+                Integer cur = queue.pollFirst();
+                if (cnt > 5) cnt = 1;
+                if (cur - cnt <= 0) {
+                    queue.add(0);
+                    break;
+                }
+                queue.addLast(cur - cnt++);
+            }
+
+            sb.append("#").append(test_case).append(" ");
+            while (!queue.isEmpty()) {
+                sb.append(queue.poll()).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
