b90e770fc2089d6b8b72aa414b543f438556955e
Dohun Lee <nestour95@gmail.com>
2023-08-10 12:18:47 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 300 ms, Memory: 25148 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
@@ -16,32 +16,33 @@ public class Main {
 
             int N = Integer.parseInt(br.readLine());
 
-            Node[] nodes = new Node[N + 2];
+            int[][] arr = new int[N + 2][2];
 
             for (int i = 0; i < N + 2; i++) {
                 st = new StringTokenizer(br.readLine());
-                nodes[i] = new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+                arr[i][0] = Integer.parseInt(st.nextToken());
+                arr[i][1] = Integer.parseInt(st.nextToken());
+                
             }
 
             // 다익스트라로 풀기
-
             // 간선 리스트 만들기
             ArrayList<Edge>[] adjList = new ArrayList[N + 2];
             for (int i = 0; i < N + 2; i++) {
                 adjList[i] = new ArrayList<>();
             }
             for (int i = 0; i <= N; i++) {
-                for (int j = i + 1; j <= N + 1; j++) {
-                    adjList[i].add(new Edge(j, Math.abs(nodes[i].x - nodes[j].x) + Math.abs(nodes[i].y - nodes[j].y)));
+                for (int j = i + 1; j < adjList.length; j++) {
+                    adjList[i].add(new Edge(j, Math.abs(arr[i][0] - arr[j][0]) + Math.abs(arr[i][1] - arr[j][1])));
                     if (i != 0) {
-                        adjList[j].add(new Edge(i, Math.abs(nodes[i].x - nodes[j].x) + Math.abs(nodes[i].y - nodes[j].y)));
+                        adjList[j].add(new Edge(i, Math.abs(arr[i][0] - arr[j][0]) + Math.abs(arr[i][1] - arr[j][1])));
                     }
                 }
             }
 
             // 거리 배열 초기화
-            int[] distance = new int[N + 2];
-            for (int i = 0; i < N + 2; i++) {
+            int[] distance = new int[arr.length];
+            for (int i = 0; i < distance.length; i++) {
                 distance[i] = Integer.MAX_VALUE;
             }
 
@@ -71,7 +72,7 @@ public class Main {
 
         }
         System.out.println(sb);
-        
+
     }
 
     static class Edge {
