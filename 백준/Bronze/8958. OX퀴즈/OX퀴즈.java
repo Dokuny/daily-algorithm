2e1c58f0e30780d9e42142ad4a28b80a595c8383
Dohun Lee <nestour95@gmail.com>
2023-08-11 10:15:30 +0900

    [Bronze II] Title: OX퀴즈, Time: 92 ms, Memory: 11616 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+
+            int sum = 0;
+            int cnt = 0;
+
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(j) == 'O') {
+                    cnt++;
+                    sum += cnt;
+                } else {
+                    cnt = 0;
+                }
+            }
+            sb.append(sum).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
