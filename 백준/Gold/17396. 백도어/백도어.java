8cfe3a5ad3bb06582069b5d1846ccbb75e60a9d6
Dohun Lee <nestour95@gmail.com>
2023-08-15 12:40:29 +0900

    [Gold V] Title: 백도어, Time: 1236 ms, Memory: 149892 KB -BaekjoonHub

--- 백준/Gold/17396. 백도어/백도어.java
+++ 백준/Gold/17396. 백도어/백도어.java
@@ -12,7 +12,6 @@ public class Main {
 
 	static ArrayList<Edge>[] adjList;
 	static long[] distances;
-	static boolean[] visited;
 
 	public static void main(String[] args) throws IOException {
 
@@ -23,58 +22,38 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 
-		// 시야 배열 초기화
 		st = new StringTokenizer(br.readLine());
-		boolean[] inSight = new boolean[N];
-		for (int i = 0; i < N; i++) {
-			inSight[i] = st.nextToken().equals("1");
-		}
-		inSight[N - 1] = false; // 넥서스는 시야가 보이더라도 갈 수 있으므로 제외
-
-		// 다익스트라 - ElogV : 600000 * 16 = 960만 정도
-
-		// 인접 리스트 작성
+		
+		
+		// 간접 리스트 만들기
 		adjList = new ArrayList[N];
 		for (int i = 0; i < N; i++) {
-			adjList[i] = new ArrayList<>();
+			if (st.nextToken().equals("0")) { // 시야가 존재하는 곳은 간선이 필요가 없음
+				adjList[i] = new ArrayList<>();
+			}
 		}
+		
+		adjList[N - 1] = new ArrayList<>();
+		
 
+		// 다익스트라 - ElogV : 600000 * 16 = 960만 정도
+		
 		// 간선 정보 입력받기
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
+			long w = Long.parseLong(st.nextToken());
 
 			// 시야가 보이는 곳이라면 굳이 넣을 필요가 없음
-			if(inSight[a] || inSight[b]) continue;
-
-			// 한쪽이 시작 노드라면 시작 노드는 양방향 X
-			if (a == 0) {
-				adjList[a].add(new Edge(b, w));
-				continue;
-			} else if (b == 0) {
-				adjList[b].add(new Edge(a, w));
-				continue;
-			}
-
-			// 한쪽이 도착 노드라면 도착 노드 양방향 X
-			if (a == N - 1) {
-				adjList[b].add(new Edge(a, w));
-				continue;
-			} else if (b == N - 1) {
-				adjList[a].add(new Edge(b, w));
-				continue;
-			}
-
-			// 그 외에는 양방향
+			if(adjList[a] == null || adjList[b] == null) continue;
+			
 			adjList[a].add(new Edge(b, w));
 			adjList[b].add(new Edge(a, w));
 		}
 
 		// 다익스트라 준비
-		visited = new boolean[N];
 		distances = new long[N];
 		Arrays.fill(distances, Long.MAX_VALUE);
 		distances[0] = 0;
@@ -86,19 +65,18 @@ public class Main {
 		while (!pq.isEmpty()) {
 			Edge cur = pq.poll();
 
-			if(visited[cur.to]) continue;
-
-			visited[cur.to] = true;
+			// 기록된 시간보다 현재 간선 시간이 더 길면 할 필요가 없음
+			if(distances[cur.to] < cur.time) continue;
 
 			for (Edge adj : adjList[cur.to]) {
-				if(visited[adj.to]) continue;
-
-				distances[adj.to] = Math.min(distances[adj.to], distances[cur.to] + adj.time);
-
-				pq.add(new Edge(adj.to, distances[adj.to]));
+				long newTime = distances[cur.to] + adj.time;
+				
+				// 기존 시간보다 새로운 시간이 더 짧으면
+				if (distances[adj.to] > newTime) {
+					distances[adj.to] = newTime;
+					pq.add(new Edge(adj.to, newTime));
+				}
 			}
-
-			adjList[cur.to] = null;
 		}
 
 		System.out.println(distances[N - 1] == Long.MAX_VALUE ? -1 : distances[N - 1]);
