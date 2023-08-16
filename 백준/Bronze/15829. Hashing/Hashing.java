02d199ba495bac61633d0185408e6da67bd3c7de
Dohun Lee <nestour95@gmail.com>
2023-08-16 19:44:37 +0900

    [Bronze II] Title: Hashing, Time: 76 ms, Memory: 11472 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -4,30 +4,30 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int L = Integer.parseInt(br.readLine());
+		int L = Integer.parseInt(br.readLine());
 
-        int r = 31;
-        int m = 1234567891;
+		int r = 31;
+		long m = 1234567891;
 
-        int[] rMod = new int[51];
-        rMod[0] = 1;
+		long[] rMod = new long[51];
+		rMod[0] = 1;
 
-        for (int i = 1; i <= 50; i++) {
-            rMod[i] = rMod[i - 1] * r % m;
-        }
+		for (int i = 1; i <= 50; i++) {
+			rMod[i] = (rMod[i - 1] % m * r % m) % m;
+		}
 
-        String input = br.readLine();
+		String input = br.readLine();
 
-        long sum = 0;
-        for (int i = 0; i < input.length(); i++) {
-            sum += (long) (input.charAt(i) - 'a' + 1) * rMod[i] % m;
-        }
-        System.out.println(sum);
+		long sum = 0;
+		for (int i = 0; i < input.length(); i++) {
+			sum += ((long) (input.charAt(i) - 'a' + 1) % m * rMod[i]) % m;
+		}
+		System.out.println(sum);
 
 
-    }
+	}
 }
 
