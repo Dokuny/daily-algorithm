d871ca1ece18318cacb9553ce26e142716697f34
Dohun Lee <nestour95@gmail.com>
2023-08-21 21:09:11 +0900

    [Silver I] Title: 회의실 배정, Time: 628 ms, Memory: 47980 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		ArrayList<Time> times = new ArrayList<>();
+		StringTokenizer st;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+
+			times.add(new Time(start, end));
+		}
+		times.sort((o1, o2) -> {
+			if (o1.end == o2.end) {
+				return Integer.compare(o1.start, o2.start);
+			}
+			return Integer.compare(o1.end, o2.end);
+		});
+
+		int cnt = 0;
+
+		int curEnd = 0;
+		for (int i = 0; i < times.size(); i++) {
+			Time time = times.get(i);
+			if(time.start < curEnd) continue;
+			cnt++;
+			curEnd = time.end;
+		}
+		System.out.println(cnt);
+
+
+	}
+
+	static class Time {
+		int start;
+		int end;
+
+		public Time(int start, int end) {
+			this.start = start;
+			this.end = end;
+		}
+	}
+}
