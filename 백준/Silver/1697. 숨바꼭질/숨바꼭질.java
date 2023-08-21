572c74ef26df934cf154540cec680647e70da512
Dohun Lee <nestour95@gmail.com>
2023-08-21 12:10:47 +0900

    [Silver I] Title: 숨바꼭질, Time: 256 ms, Memory: 101032 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static int N;
+    static int K;
+    static int answer;
+
+    public static void main(String args[]) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+
+        int[] dp = new int[200001];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+
+
+        Queue<Node> queue = new ArrayDeque<>();
+
+        queue.add(new Node(N, 0));
+
+        answer = Math.abs(N - K);
+
+        while (!queue.isEmpty()) {
+            Node cur = queue.poll();
+
+            dp[cur.pos] = Math.min(dp[cur.pos], cur.cnt);
+
+            int teleport = cur.pos * 2;
+            int forward = cur.pos + 1;
+            int back = cur.pos - 1;
+
+            int newCnt = cur.cnt + 1;
+            if(teleport <= 200000 && dp[teleport] > newCnt){
+                queue.add(new Node(cur.pos * 2 , newCnt));
+            }
+            if(forward <= 200000 && dp[forward] > newCnt){
+                queue.add(new Node(cur.pos + 1, newCnt));
+            }
+            if(back > -1 && dp[back] > newCnt){
+                queue.add(new Node(cur.pos - 1, newCnt));
+            }
+        }
+
+        System.out.println(dp[K]);
+    }
+
+    static class Node {
+        int pos;
+        int cnt;
+
+        public Node(int pos, int cnt) {
+            this.pos = pos;
+            this.cnt = cnt;
+        }
+    }
+
+
+}
+
