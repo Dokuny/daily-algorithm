8d529295c7ade0cb89c28a426f41cf1d00e8848a
Dohun Lee <nestour95@gmail.com>
2023-08-23 13:32:45 +0900

    [D4] Title: 최소 스패닝 트리, Time: 1,850 ms, Memory: 125,600 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.StringTokenizer;
 
@@ -29,25 +30,21 @@ class Solution {
 				parents[i] = i;
 			}
 
-			ArrayList<Edge> edges = new ArrayList<>();
+			int[][] edges = new int[E][3];
 			for (int i = 0; i < E; i++) {
 				st = new StringTokenizer(br.readLine());
 
-				int a = Integer.parseInt(st.nextToken());
-				int b = Integer.parseInt(st.nextToken());
-				int c = Integer.parseInt(st.nextToken());
-
-				edges.add(new Edge(a, b, c));
-				edges.add(new Edge(b, a, c));
+				edges[i][0] = Integer.parseInt(st.nextToken());
+				edges[i][1] = Integer.parseInt(st.nextToken());
+				edges[i][2]= Integer.parseInt(st.nextToken());
 			}
-			edges.sort(Comparator.comparingInt(o -> o.w));
+			Arrays.sort(edges, Comparator.comparingInt(o -> o[2]));
 
 			long weightSum = 0;
-			for (int i = 0; i < edges.size(); i++) {
-				Edge edge = edges.get(i);
-				if (find(edge.a) != find(edge.b)) {
-					union(edge.a, edge.b);
-					weightSum += edge.w;
+			for (int i = 0; i < edges.length; i++) {
+				if (find(edges[i][0]) != find(edges[i][1])) {
+					union(edges[i][0], edges[i][1]);
+					weightSum += edges[i][2];
 				}
 			}
 			result.append("#").append(test_case).append(" ").append(weightSum).append("\n");
