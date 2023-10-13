d56ce737b0a56377fe255707ba3b50083f9e82e4
Dohun Lee <nestour95@gmail.com>
2023-10-13 12:05:54 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,2 @@
+-- 코드를 입력하세요
+SELECT concat('/home/grep/src/',ugf.board_id,'/',ugf.file_id,ugf.file_name,ugf.file_ext) as file_path from used_goods_file as ugf where ugf.board_id = (select board_id from used_goods_board order by views desc limit 1) order by ugf.file_id desc;
